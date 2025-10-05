498d942894247e8d7130db3044fc65ccc0cdf54e
thanosda <154046417+Thanosda@users.noreply.github.com>
2025-10-06 00:35:34 +0530

    finished

--- todoApp/src/ToDoList.jsx
+++ todoApp/src/ToDoList.jsx
@@ -1,20 +1,55 @@
 import React, { useState } from "react";
 
 function ToDoList() {
-  const [task, setTask] = useState([]);
+  const [task, setTask] = useState(["Wake Up:","Bath:","Eat:","Games:","Sleep:"]);
   const [newtsk, setnewtask] = useState("");
 
-  function handleInoChan(event) {}
-  function AddNEwTask() {}
-  function deleteTask(index) {}
-  function MoveTaskUp(index) {}
-  function MoveTaskDown(index) {}
+  function handleInoChan(event) {
+    setnewtask(event.target.value);
+  }
+  function AddNEwTask() {
+    if (newtsk.trim() !== "") {
+      setTask([...task, newtsk]);
+      setnewtask("");
+    }
+  }
+  function deleteTask(index) {
+    const updatedTasks = task.filter((_, i) => i !== index);
+    setTask(updatedTasks);
+
+  }
+  function MoveTaskUp(index) {
+    if (index > 0) {
+      const newTasks = [...task];
+      [newTasks[index - 1], newTasks[index]] = [newTasks[index], newTasks[index - 1]];
+      setTask(newTasks);
+    }
+  }
+  function MoveTaskDown(index) {
+    if (index < task.length - 1) {
+      const newTasks = [...task];
+      [newTasks[index + 1], newTasks[index]] = [newTasks[index], newTasks[index + 1]];
+      setTask(newTasks);
+    }
+  }
 
   return (
     <>
       <div className="ToDoList">
-        <h1>To Do List</h1>
-        
+        <h1 className="htag">To Do List</h1>
+        <input type="text" value={newtsk} onChange={handleInoChan} placeholder="Write Somethings" />
+        <button className="add" onClick={AddNEwTask}>Add</button>
+
+         <ol className="list">
+          {task.map((tsk, index) => (
+            <li  className="hell" key={index}>
+              <span className="text">{tsk}</span>
+              <button onClick={()=>deleteTask(index)}>Delete</button>
+              <button onClick={()=>MoveTaskUp(index)}>Up</button>
+              <button onClick={()=>MoveTaskDown(index)}>Down</button>
+             </li>
+          ))}
+         </ol>
       </div>
     </>
   );
